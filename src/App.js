987d4c877bbe44e200c08f7dc5e124aca56dc424
ginotria <romanangelo.tria@gmail.com>
2017-02-14 01:08:08 +0800

    * added searchBox

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList.js';
+import SearchBox from './SearchBox'
 
+var libraries = [
+    { name: 'Backbone.js', url: 'http://documentcloud.github.io/backbone/'},
+    { name: 'AngularJS', url: 'https://angularjs.org/'},
+    { name: 'jQuery', url: 'http://jquery.com/'},
+    { name: 'Prototype', url: 'http://www.prototypejs.org/'},
+    { name: 'React', url: 'http://facebook.github.io/react/'},
+    { name: 'Ember', url: 'http://emberjs.com/'},
+    { name: 'Knockout.js', url: 'http://knockoutjs.com/'},
+    { name: 'Dojo', url: 'http://dojotoolkit.org/'},
+    { name: 'Mootools', url: 'http://mootools.net/'},
+    { name: 'Underscore', url: 'http://documentcloud.github.io/underscore/'},
+    { name: 'Lodash', url: 'http://lodash.com/'},
+    { name: 'Moment', url: 'http://momentjs.com/'},
+    { name: 'Express', url: 'http://expressjs.com/'},
+    { name: 'Koa', url: 'http://koajs.com/'},
+];
 class App extends Component {
   state = {
     name: ''
@@ -13,9 +30,12 @@ class App extends Component {
         <div className="App-header">
           <h2>React Seatwork</h2>
         </div>
-        <p className="App-intro">
+        <div className="App-intro">
           <TodoList />
-        </p>
+        </div>
+        <div>
+          <SearchBox items={libraries} />
+        </div>
       </div>
     );
   }
