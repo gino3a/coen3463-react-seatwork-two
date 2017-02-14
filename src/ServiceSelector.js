731f94f232c9c1265cd1a089d9aa5e8b93e7d90c
ginotria <romanangelo.tria@gmail.com>
2017-02-14 08:59:07 +0800

    update ServiceSelector instruction

--- src/ServiceSelector.js
+++ src/ServiceSelector.js
@@ -15,7 +15,6 @@ class ServiceSelector extends Component {
         <h3>Order Form</h3>
         <div className='Service-list'> </div>
         <div className='Service-total'>
-          <div id="total">Total <b>${this.state.total.toFixed(2)}</b></div>
         </div>
       </div>
     );
